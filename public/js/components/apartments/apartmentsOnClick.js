4d2953a40d267659def916a75290d8877b27ad23
dominik2323 <dominik.tomcik23@gmail.com>
2022-07-26 01:19:52 +0200

    minor

--- public/js/components/apartments/apartmentsOnClick.js
+++ public/js/components/apartments/apartmentsOnClick.js
@@ -1,10 +1,7 @@
 import getUrlParams from "../../helpers/getUrlParams.js";
 
-const params = getUrlParams();
-
 function apartmentsOnClick(e, emittor) {
-  // /byty/a1-24/
-  console.log(emittor);
+  const params = getUrlParams();
   if (!emittor.hasAttribute("data-emittor-inactive")) {
     window.location = `https://www.residenceuskolky.cz/byty/${params.building}-${emittor.dataset.name}`;
   }
